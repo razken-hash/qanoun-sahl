e0d6526ad4306759b0c6f83c4e45b22f9d4cff8d
razken-hash <ka_kenniche@esi.dz>
2024-04-09 22:37:57 +0100

    Project Init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const QanounSahlApp(),
+  );
+}
+
+class QanounSahlApp extends StatelessWidget {
+  const QanounSahlApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
